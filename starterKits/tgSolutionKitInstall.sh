b93e4e930e784cbe610e3195d2a3efd5cd62f2f3
bhardaway <robert.hardaway@gmail.com>
2022-03-24 10:51:29 -0600

    scripting

---
+++ starterKits/tgSolutionKitInstall.sh
@@ -0,0 +1,147 @@
+#!/bin/bash
+
+###############################################
+# Copyright (c)  2015-now, TigerGraph Inc.
+# All rights reserved
+# Solution pack for TigerGraph Pre Sales
+# Author: robert.hardaway@tigergraph.com
+################################################
+
+## This script will will install an individual starter kit
+
+echo 'This installer will install any starter kit on any tg instance'
+echo ''
+echo 'Install options are:'
+echo '   1. GSQL - Install via gsql command line to a local'
+echo '   3. Python - Install via python script and pyTigerGraph library to a remote instance'
+echo ''
+echo '     NOTE: Setup on TGCloud is only available thru python install'
+echo ''
+
+chadifier="   - "
+while true; do
+	KITNUMBER=1
+	for file in "./gsql/"create-schema*.gsql
+	do
+		IFS='-'
+		if [[ -f "$file" ]] ; then
+			read -ra ADDR <<< "$file"
+
+			kitNumArray+=( ${KITNUMBER} )
+			kitNameArray+=( ${ADDR[2]::${#ADDR[2]}-10} )
+			if [[ ${KITNUMBER} -gt 9 ]]; then
+				echo "  ${KITNUMBER}  -  ${ADDR[2]::${#ADDR[2]}-10} "
+			else
+				echo "  ${KITNUMBER}   -  ${ADDR[2]::${#ADDR[2]}-10} "
+			fi
+			((KITNUMBER=KITNUMBER+1))
+		fi
+	done
+
+	echo ''
+	echo "choose the kit you want to work with"
+	read -p "     Valid kits are 1 thru ${KITNUMBER}: " kitNumber
+
+	## make sure we got a valid number
+	if [ -n "$kitNumber" ] && [ "$kitNumber" -eq "$kitNumber" ] 2>/dev/null; then
+		echo ''
+		echo "starter kit choosen is: " ${kitNumArray[kitNumber-1]}
+		echo " corresponding to kit: " ${kitNameArray[kitNumber-1]}
+		break
+	else
+  		echo "Needs to be a number between 1 and ${KITNUMBER}"
+  		echo ''
+	fi
+done
+
+while true; do
+	echo ''
+	echo 'What would you like to do today?'
+	echo '   C/c - create kit schema'
+	echo '   D/d - create the S3 data source - NOTE: in order to load data, you need a data source'
+	echo '   L/l - create a load job for a kit'
+	echo '   R/r - run a load job'
+	echo '   A/a - Do all 3, create schema and job, and run load for a kit'
+	read -p "Answer: " tg_task
+	if [ -z "$tg_task" ];
+	then 
+		echo 'Response needs to be c/d/l/r'
+	elif [ $tg_task == 'C' ] || [ $tg_task == 'c' ]; then
+		fn='create-schema'
+		break
+	elif [ $tg_task == 'D' ] || [ $tg_task == 'd' ]; then
+		fn='tg_createDataSource.py'
+		break
+	elif [ $tg_task == 'L' ] || [ $tg_task == 'l' ]; then
+		fn='create-load-job'
+		break
+	elif [ $tg_task == 'R' ] || [ $tg_task == 'r' ]; then
+		fn='run-load-job'
+		break
+	elif [ $tg_task == 'A' ] || [ $tg_task == 'a' ]; then
+		fn='all'
+		break
+	else
+		echo 'Invalid entry, nmeeds to be c/d/l/r or C/D/L/R'
+	fi
+done
+
+while true; do
+	echo ''
+	read -p "Do you want to install via GSQL or Python? (G/P/p): " install_type
+	if [ -z "$install_type" ];
+	then 
+		echo "No input provided: should be create/load"
+	elif [ $install_type == 'G' ] || [ $install_type == 'g' ] || [ $install_type == 'gsql' ]; then
+		echo "Cool, gsql it is..."
+		echo "executing this command: gsql ./gsql/${fn}-${kitNameArray[kitNumber-1]}Graph.gsql"
+	##	gsql "./gsql/${fn}-${kitNameArray[kitNumber-1]}Graph.gsql"
+		break
+	elif [ $install_type == 'P' ] || [ $install_type == 'p' ] || [ $install_type == 'python' ]; then
+		echo "Cool, python it is..."
+		echo "Lets make sure python is installed and configured"
+		python --version
+		result=$?
+		if [[ $result=0 ]]; then
+			echo 'Python installed, checking version'
+		else
+			echo 'Python is required to install to TGCloud, but not for local installs thru gsql...'
+				read -p "     Would you like to continue (Y), or exit and get your python together (N): " yesOrNo
+				if [[ $yesOrNo == 'N' ]] || [[ $yesOrNo == 'n' ]]; then
+					echo 'Fine, try again once python *and the pyTigerGraph library are installed'
+					exit 0
+				elif [[ $yesOrNo == 'Y' ]] || [[ $yesOrNo == 'y' ]]; then
+					echo "Lets keep going..."
+					echo ''
+				else
+					echo 'I need a Yes or No'
+					exit 0
+				fi
+		fi
+		echo 'Update the connection props...'
+		./updateConnProps.sh
+		echo ''
+		if [[ $fn == 'tg_createDataSource.py' ]]; then
+			echo "executing the create data source command: python ./py/${fn}"
+			python ./py/tg_createDataSource.py
+		else
+			echo "executing this command: python ./py/${fn}-${kitNameArray[kitNumber-1]}Graph.py"
+			python "./py/${fn}-${kitNameArray[kitNumber-1]}Graph.py"
+		fi
+		echo '' 
+		break
+	else
+		echo "Invalid input provided: should be g/p, try again"
+	fi
+done
+
+echo ''
+echo "Starter kit ${kitNameArray[kitNumber-1]} setup is complete"
+echo ''
+
+
+
+
+
+
+
---
+++ starterKits/updateConnProps.sh
@@ -0,0 +1,164 @@
+#!/bin/bash
+
+###############################################
+# Copyright (c)  2015-now, TigerGraph Inc.
+# All rights reserved
+# Solution pack for TigerGraph Pre Sales
+# Author: robert.hardaway@tigergraph.com
+################################################
+
+## Get TG connection info
+
+props_file='./tg.properties'
+if [ -f "$props_file" ]
+then
+  while IFS='=' read -r key value
+  do
+    key=$(echo $key | tr '.' '_')
+    eval ${key}=\${value}
+  done < "$props_file"
+else
+  echo "$file file not found, using defaults"
+  ## Default to local docker
+  tg_host='localhost'
+  tg_protocol="http"
+  tg_username="tigergraph"
+  tg_password="tigergraph"
+  tg_s3_data_source="s3_data_source"
+  tg_s3_bucket_name="tg-workshop-us"
+  tg_access_key_ID="AKIA45R*********"
+  tg_secret_access_key="jeO8GXIVCpjDkYVccHfuLL**************"
+fi
+
+echo ''
+echo 'The current property settings are:'
+echo ''
+echo "tg_host = $tg_host"
+echo "tg_protocol = $tg_protocol"
+echo "tg_username = $tg_username"
+echo "tg_password = $tg_password"
+echo "tg_s3_data_source = $tg_s3_data_source"
+echo "tg_s3_bucket_name = $tg_s3_bucket_name"
+echo "tg_access_key_ID = $tg_access_key_ID"
+echo "tg_secret_access_key = $tg_secret_access_key"
+echo ''
+
+read -p "Do you want to modify any of these properties? (Y/n) " updateProps
+if [[ "$updateProps" == *"Y"* || "$updateProps" == *"y"* || "$updateProps" == *"Yes"* || "$updateProps" == *"yes"* ]]; then
+    echo "Updating the props..."
+else
+    echo "Continuing with current props..."
+    echo ''
+    exit 0
+fi
+
+echo 'Enter the connection info for the TigerGraph instance. Hit enter for no change'
+echo ''
+read -p "tg_host <$tg_host>: " new_host
+if [[ ! -z $new_host ]];
+then 
+    tg_host_new=$new_host
+else
+    tg_host_new=$tg_host
+fi
+echo ''
+read -p "tg_protocol <$tg_protocol>: " new_tg_protocol
+if [[ ! -z $new_tg_protocol ]];
+then 
+    tg_protocol_new=$new_tg_protocol
+else
+    tg_protocol_new=$tg_host
+fi
+echo ''
+read -p "tg_username <$tg_username>: " new_username
+if [[ ! -z $new_username ]];
+then 
+    tg_username_new=$new_username
+else
+    tg_username_new=$tg_username
+fi
+echo ''
+read -p "tg_password <$tg_password>: " new_password
+if [[ ! -z $new_password ]];
+then 
+    tg_password_new=$new_password
+else
+    tg_password_new=$tg_password
+fi
+echo ''
+read -p "tg_s3_data_source <$tg_s3_data_source>: " new_tg_s3_data_source
+if [[ ! -z $new_tg_s3_data_source ]];
+then 
+    tg_datasource_new=$new_tg_s3_data_source
+else
+    tg_datasource_new=$tg_s3_data_source
+fi
+echo ''
+echo 'Dont change the bucket name unless the new bucket has been built to contain the datasets'
+echo ''
+read -p "tg_s3_bucket_name <$tg_s3_bucket_name>: " new_tg_s3_bucket_name
+if [[ ! -z $new_tg_s3_data_source ]];
+then 
+    tg_bucketname_new=$new_tg_s3_bucket_name
+else
+    tg_bucketname_new=$tg_s3_bucket_name
+fi
+echo ''
+read -p "tg_access_key_ID <$tg_access_key_ID>: " new_tg_access_key_ID
+if [[ ! -z $new_tg_access_key_ID ]];
+then 
+    tg_access_key_ID_new=$new_tg_access_key_ID
+else
+    tg_access_key_ID_new=$tg_access_key_ID
+fi
+echo ''
+read -p "tg_secret_access_key <$tg_secret_access_key>: " new_tg_secret_access_key
+if [[ ! -z $new_tg_secret_access_key ]];
+then 
+    tg_secret_access_key_new=$new_tg_secret_access_key
+else
+    tg_secret_access_key_new=$tg_secret_access_key
+fi
+
+echo ''
+echo 'NOTE: these credentails will be written to the python load scripts in clear text'
+echo ''
+
+echo "Host new is: $tg_host_new"
+echo "Host is: $tg_host"
+
+echo ''
+echo 'Configure the S3 access token'
+echo ''
+
+## add the keys to the load job template
+sed "s/ACCESSKEYID/${access_key_id}/g" ./py/tg_createDataSource_orig.py > bob.tmp
+sed 's/SECRETACCESSKEY/${secret_access_key}/g' bob.tmp > ./py/tg_createDataSource.py
+rm -rf bob.tmp
+
+## Write new props to tg.properties file
+echo "update the props filename"
+echo "tg_host=$tg_host_new" > ./tg.properties
+echo "tg_protocol=$tg_protocol_new" >> ./tg.properties
+echo "tg_username=$tg_username_new" >> ./tg.properties
+echo "tg_password=$tg_password_new" >> ./tg.properties
+echo "tg_s3_data_source=$tg_datasource_new" >> ./tg.properties
+echo "tg_s3_bucket_name=$tg_bucketname_new" >> ./tg.properties
+echo "tg_access_key_ID=$tg_access_key_ID_new" >> ./tg.properties
+echo "tg_secret_access_key=$tg_secret_access_key_new" >> ./tg.properties
+
+## Replace tokens in the template with actual values
+for file in "./py/"*.py
+  do
+    echo "adding props to file: $file"
+    sed "s/${tg_host}/${tg_host_new}/g" $file > bob.tmp
+    sed "s/${tg_username}/${tg_username_new}/g" bob.tmp > bob2.tmp
+    sed "s/${tg_password}/${tg_password_new}/g" bob2.tmp > bob3.tmp
+    sed "s/${tg_protocol}/${tg_protocol_new}/g" bob3.tmp > bob4.tmp
+
+    mv bob4.tmp $file
+    rm -rf bob*.tmp
+  done
+
+echo ''
+echo 'Python scripts updated to reflect connection info..'
