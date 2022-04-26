3383323bc8d1eff109f37ac8977064a02cf997fc
bhardaway <robert.hardaway@gmail.com>
2022-04-26 13:21:10 -0600

    scripts somemore

--- packages/cust360/installCust360.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-gsql packages/cust360/scripts/create_cust360_schema.gsql
-gsql packages/cust360/scripts/customer360_load_er.gsql
-gsql packages/cust360/scripts/customer360_load_camp.gsql
-gsql packages/cust360/scripts/customer360_load_events.gsql
-gsql packages/cust360/scripts/customer360_load_orders.gsql
-gsql packages/cust360/scripts/customer360_load_ratings.gsql
