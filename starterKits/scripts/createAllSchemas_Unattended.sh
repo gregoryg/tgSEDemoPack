834526ace38fb08d174d28e57dafeb9a205f9eda
bhardaway <robert.hardaway@gmail.com>
2022-05-01 09:54:22 -0600

    props

--- starterKits/scripts/createAllSchemas_Unattended.sh
+++ starterKits/scripts/createAllSchemas_Unattended.sh
@@ -9,7 +9,7 @@
 
 ## This scripts will create schemas and load data from S3, depending on input
 
-echo 'This installer will create all of the public starter kits on any tg instance'
+echo 'This installer will create schemas for all of the public starter kits on any tg instance'
 echo ''
 echo 'The options are:'
 echo '   1. Install via gsql command line'
@@ -18,33 +18,90 @@ echo ''
 echo 'NOTE: Setup on TGCloud is onlt available thru python'
 echo ''
 
-while true; do
+if [ ! -f ./.prechk ]
+then
 	echo ''
-	echo 'What would you like to do today?'
-	echo '   C/c - create the schemas'
-	echo '   D/d - create the S3 data source'
-	echo '   L/l - create the load jobs'
-	echo '   R/r - run the load jobs'
-	read -p "Answer: " tg_task
-	if [ -z "$tg_task" ];
-	then 
-		echo 'Response needs to be c/d/l/r'
-	elif [ $tg_task == 'C' ] || [ $tg_task == 'c' ]; then
-		fn='create-schema'
-		break
-	elif [ $tg_task == 'D' ] || [ $tg_task == 'd' ]; then
-		fn='tg_createDataS'
-		break
-	elif [ $tg_task == 'L' ] || [ $tg_task == 'l' ]; then
-		fn='create-load-job'
-		break
-	elif [ $tg_task == 'R' ] || [ $tg_task == 'r' ]; then
-		fn='run'
-		break
-	else
-		echo 'Invalid entry, nmeeds to be c/d/l/r or C/D/L/R'
-	fi
-done
+	echo "Need to run the pre-checks first"
+	exit 22
+fi
+
+props_file='./tg.properties'
+if [ -f "$props_file" ]
+then
+  while IFS='=' read -r key value
+  do
+    key=$(echo $key | tr '.' '_')
+    eval ${key}=\${value}
+  done < "$props_file"
+else
+	echo 'Need to set the properties first by running the installer script'
+	exit 11
+fi
+
+install_type=$1
+echo "Will install using method $install_type"
+
+if [ $install_type == 'p' ] || [ $install_type == 'P' ] || [ $install_type == 'python' ]; then
+	echo "create schemas...."
+	for file in "./scripts/py/"create-schema-*.py
+	do
+		if [[ -f "$file" ]] ; then
+		echo "running script: $file"
+		##python $file
+		fi
+	done
+	echo "create data source...."
+	## gsql p $tg_password ./scripts/py/tg_createDataSource.py
+
+	echo "create load jobs...."
+	for file in "./scripts/py/"create-load-*.py
+	do
+		if [[ -f "$file" ]] ; then
+		echo "running script: $file"
+		##python $file
+		fi
+	done
+	echo "run load jobs...."
+	for file in "./scripts/py/"run-load-job-*.py
+	do
+		if [[ -f "$file" ]] ; then
+		echo "running script: $file"
+		##python $file
+		fi
+	done
+elif [ $install_type == 'G' ] || [ $install_type == 'g' ] || [ $install_type == 'gsql' ]; then
+	echo "create schemas...."
+	for file in "./scripts/gsql/"create-schema-*.gsql
+	do
+		if [[ -f "$file" ]] ; then
+		echo "running script: $file"
+		##gsql -p $tg_password $file
+		fi
+	done
+	echo "create data source.... gsql password is $tg_password"
+	## gsql p $tg_password ./scripts/gsql/tg_createDataSource.gsql
+
+	echo "create load jobs...."
+	for file in "./scripts/gsql/"create-load-*.gsql
+	do
+		if [[ -f "$file" ]] ; then
+		echo "running script: $file"
+		##gsql -p $tg_password $file
+		fi
+	done
+	echo "run load jobs...."
+	for file in "./scripts/gsql/"run-load-job-*.gsql
+	do
+		if [[ -f "$file" ]] ; then
+		echo "running script: $file"
+		##gsql -p $tg_password $file
+		fi
+	done
+
+else
+	echo 'The install method needs to be p/python or g/gsql, please specifiy'
+	exit 33
+fi
 
 while true; do
 	echo ''
